fdc3748cd609ea43c9843aaf0083c218b502be5a
siewertserau <siewerts@erau.edu>
2014-10-14 16:23:40 -0700

    Example Code use in Lecture in SE420
    Code examples to explore path coverage, use of gcov, etc. for SE420 at
    ERAU Prescott

---
+++ Simple-Gcov/simple.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(void)
+{
+    int x;
+
+    if(x > 0)
+    {
+        printf("TRUE\n");
+    }
+    else
+    {
+        printf("FALSE\n");
+    }
+
+    exit(1);
+}
