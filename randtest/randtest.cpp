9199f78059961d635919eb279e378c05d557b021
SIEWERTS <siewerts@prcoesevs1.localdomain>
2018-01-25 06:41:39 -0700

    Updated for scaling of bins and iterations with some improvements to format of output.

--- randtest/randtest.cpp
+++ randtest/randtest.cpp
@@ -1,13 +1,16 @@
 #include <cmath>
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <stdlib.h>
 using namespace std;
 
 
-#define EXPECTED_COUNTS (20)
-#define ITERATIONS (1000)
-#define BINS (50)
+#define ITERATIONS (10000)
+#define BINS (100)
+
+// Assuming a uniform, equiprobable distribution of outcomes
+#define EXPECTED_COUNTS (ITERATIONS/BINS)
 
 static unsigned int randcnt[BINS];
 
@@ -17,17 +20,19 @@ int main(void)
     unsigned int seed;
     unsigned int sum=0;
     int diff;
-    int bin, idx, jdx;
+    int bin, idx, jdx, histLength;
     double ave=0.0, var=0.0, std=0.0;
     ofstream fout;
+
+    histLength = (int)(1.3 * EXPECTED_COUNTS);
 
     for(idx=0; idx < BINS; idx++) randcnt[idx]=0;
 
     do
     {
         cout << "Built-in Random number generator with range 0..." << RAND_MAX
-             << " will be used in scaled range 0...49\n";
-        cout << "Enter a random number seed value between 0 and 49: ";
+             << " will be used in scaled range 0..." << BINS << "\n";
+        cout << "Enter a random number seed value between 0 and " << BINS << ": ";
         cin >> seed;
         if(seed >= BINS) cerr << "ERROR - seed value out of range\n";
 
@@ -47,16 +52,17 @@ int main(void)
     // Produce the ASCII histogram and statistics for this seed
     for(idx=0; idx < BINS; idx++)
     {
-        // for formatting, keep width for output 0...49 to two spaces
-        cout.width(2);
+        // for formatting, keep width for output 000...999 to three spaces
+        cout.width(3);
 
-        cout << idx << ", " << (randcnt[idx]) << " - ";
+        cout << right << setw(3) << idx << ", " << right << setw(3) << (randcnt[idx]) << " - ";
 
         // compute sum and average for each bin
         sum += randcnt[idx];
         ave += ((double)(randcnt[idx]))/((double)BINS);
 
-        for(jdx=0; jdx < BINS; jdx++)  // This assumes that the bin count will be less than BINS
+        // This assumes that the bin count will be less than 2 x EXPECTED_COUNT
+        for(jdx=0; jdx < histLength; jdx++)
         {
             if(((unsigned int)jdx) < (randcnt[idx]))
                 cout << "*";
@@ -69,7 +75,7 @@ int main(void)
 
         // Compute difference from mean and square it and average over all bins
         var += ((double)(diff*diff)) / ((double)BINS);
-        cout << diff;
+        cout << right << setw(4) << diff;
 
         cout << "\n";
     }
@@ -97,7 +103,7 @@ int main(void)
 
         for(idx=0; idx < ITERATIONS; idx++)
         {
-            bin = rand() % BINS; // must remain in range of 0 to 49, so take modulo 50
+            bin = rand() % BINS; // must remain in range of 0 to BINS, so take modulo BINS
             (randcnt[bin])++;
         }
 
@@ -111,7 +117,7 @@ int main(void)
 
         std = sqrt(var);
 
-        fout << seed << "," << var << "," << std << "\n";
+        fout << seed << "," << ave << "," << var << "," << std << "\n";
 
     }
 
